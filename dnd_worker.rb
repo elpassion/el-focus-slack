cec53fb9ea4f9960b47433c1a4d61142beef9d9e
Arkadiusz Oleksy <oleksy.arkadiusz@gmail.com>
2017-01-13 13:13:51 +0100

    start worker per user

--- dnd_worker.rb
+++ dnd_worker.rb
@@ -1,50 +1,55 @@
 require_relative 'config/initialize'
 
-class Dnd
-  class ImList < Dnd
-    def run!
-      storage.get_users.each do |user_id, user_data|
-        access_token = user_data.fetch('access_token')
-        client = client(access_token)
-        ims = client.im_list.ims
-        ims.each do |channel|
-          SendBusyMessageWorker.perform_async(channel.id, access_token, channel.user)
+module Dnd
+  class SendBusyMessageWorker
+    include Sidekiq::Worker
+    sidekiq_options retry: false
+
+    def perform(user_id)
+      user = User.new(user_id)
+      return unless user.busy?
+      access_token = storage.get("user:#{user_id}").fetch('access_token')
+
+      client = client(access_token)
+
+      ims(client).each do |channel|
+        channel_id = channel.id
+        channel_user = channel.user
+
+        puts "Downloading for [#{channel_id}]..."
+        history = client.im_history(channel: channel_id, count: 1, unreads: 1)
+        return if history.messages.empty?
+        last_message = history.messages.last
+        last_message_user = last_message.user
+        # post_message if unread pending and last_message_user is_interlocutor?
+        if history.unread_count_display > 0 && last_message_user == channel_user
+          puts "sending message to channel #{channel_id}, user: #{last_message_user}"
+          client.chat_postMessage(channel: channel_id, text: 'This is ElPomodoro Slack App test!', as_user: true)
         end
       end
-    end
-  end
 
-  class SendBusyMessageWorker < Dnd
-    include Sidekiq::Worker
-
-    def perform(channel_id, access_token, channel_user)
-      client = client(access_token)
-      puts "Downloading for [#{channel_id}]..."
-      history = client.im_history(channel: channel_id, count: 1, unreads: 1)
-      return if history.messages.empty?
-      last_message = history.messages.last
-      last_message_user = last_message.user
-      # post_message if unread pending and last_message_user is_interlocutor?
-      if history.unread_count_display > 0 && last_message_user == channel_user
-        puts "sending message to channel #{channel_id}, user: #{last_message_user}"
-        client.chat_postMessage(channel: channel_id, text: 'This is ElPomodoro Slack App test!', as_user: true)
-      end
+      puts 'scheduling next in 20 seconds'
+      self.class.perform_in 20, user_id
     end
+
 
     private
 
     def client(access_token)
       SlackClient.new(access_token).get
     end
-  end
 
-  private
+    def ims(client)
+      client.im_list.ims
+    end
 
-  def client(access_token)
-    SlackClient.new(access_token).get
-  end
+    def busy?(user_id)
+      $storage.exists("busy:#{user_id}")
+    end
 
-  def storage
-    @storage ||= Storage.new
+    def storage
+      $storage
+    end
+
   end
 end
