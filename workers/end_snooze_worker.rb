c9ffe5d64d0362f46c1661b739430aa0b84ca185
Kacper Walanus <kacper@walanus.com>
2017-05-29 12:18:11 +0200

    [#10] do not crash if snooze is already turned off

--- workers/end_snooze_worker.rb
+++ workers/end_snooze_worker.rb
@@ -2,11 +2,16 @@ class Workers::EndSnoozeWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
+  SNOOZE_ALREADY_NOT_ACTIVE_ERROR_MESSAGE = 'snooze_not_active'
+
   def perform(user_id)
     user = User.new(user_id)
 
     client = SlackClient.for_acces_token(user.access_token)
 
     client.dnd_endSnooze
+  rescue Slack::Web::Api::Error => error
+    return if error.message == SNOOZE_ALREADY_NOT_ACTIVE_ERROR_MESSAGE
+    raise error
   end
 end
