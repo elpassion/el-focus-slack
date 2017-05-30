88f9806090caeb0bcb02e4ff1de538dfce68e41b
Kacper Walanus <kacper@walanus.com>
2017-05-30 08:25:43 +0200

    [#5] send message on missing scope

--- slack_client.rb
+++ slack_client.rb
@@ -13,6 +13,23 @@ class SlackClient
     else
       client.send(method, *args)
     end
+  rescue Slack::Web::Api::Error => error
+    case error.message
+      when 'missing_scope'
+        host = ENV['SLACK_REDIRECT_URI'].gsub('/finish_auth', '')
+        send_message_to_access_token_owner("Permission update required - please go to #{host} and click \"Add to Slack\" button to fully enjoy EL Pomodoro!")
+      else
+        raise error
+    end
+  end
+
+  def send_message_to_access_token_owner(message)
+    auth_test = client.send(:auth_test)
+    team_id = auth_test.fetch('team_id')
+    user_id = auth_test.fetch('user_id')
+    bot_access_token = $storage.get("bot:#{team_id}").fetch('access_token')
+    bot_client = self.class.create_slack_client(bot_access_token)
+    bot_client.send(:chat_postMessage, channel: user_id, as_user: false, text: message)
   end
 
   def self.for_access_token(access_token)
